151059af988d28efb3e6d5515d8f4d8290657fd9
yerimbet.aitzhanov <king_erosh@icloud.com>
2024-09-06 14:23:10 +0500

    Initial Commit

---
+++ Minigames/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Minigames
+//
+//  Created by Ерош Айтжанов on 06.09.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
