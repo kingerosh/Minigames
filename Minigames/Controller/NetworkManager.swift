dd08ac50ae5e36c180b332ef07d984d6a731c269
yerimbet.aitzhanov <king_erosh@icloud.com>
2024-09-08 21:49:41 +0500

    v1.0 with XO and EMOJI

---
+++ Minigames/Controller/NetworkManager.swift
@@ -0,0 +1,8 @@
+//
+//  NetworkManager.swift
+//  Minigames
+//
+//  Created by Ерош Айтжанов on 08.09.2024.
+//
+
+import Foundation
---
+++ Minigames/View/Subclass/MainTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  MainTableViewCell.swift
+//  Minigames
+//
+//  Created by Ерош Айтжанов on 06.09.2024.
+//
+
+import UIKit
+
+class MainTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
